659d4c7013e34fb024fd7dea3b7c09724f6caef7
Ta-p1 <panu.kan12@gmail.com>
2025-09-17 21:23:49 +0700

    fix bug

---
+++ database/migrations/2025_09_17_141945_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_url');
+            $table->string('short_url');
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->timestamp('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
